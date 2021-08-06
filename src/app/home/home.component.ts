2b955bdf822c9f4e73a9f9ac463ce00afa32f855
Sunny Wadhwani <sunny.wadhwani@asml.com>
2021-08-06 11:50:50 +0530

    Initial changes

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ArticleListConfig, TagsService, UserService } from '../core';
+import {TourService} from 'ngx-ui-tour-md-menu';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private tagsService: TagsService,
+    private userService: UserService,
+    private readonly tourService: TourService
+  ) {}
+
+  isAuthenticated: boolean;
+  listConfig: ArticleListConfig = {
+    type: 'all',
+    filters: {}
+  };
+  tags: Array<string> = [];
+  tagsLoaded = false;
+
+  ngOnInit() {
+    this.tourService.initialize([{
+      anchorId: 'welcome',
+      content: 'Some content',
+      title: 'First',
+    }]);
+
+    this.tourService.start();
+
+    this.userService.isAuthenticated.subscribe(
+      (authenticated) => {
+        this.isAuthenticated = authenticated;
+
+        // set the article list accordingly
+        if (authenticated) {
+          this.setListTo('feed');
+        } else {
+          this.setListTo('all');
+        }
+      }
+    );
+
+    this.tagsService.getAll()
+    .subscribe(tags => {
+      this.tags = tags;
+      this.tagsLoaded = true;
+    });
+  }
+
+  setListTo(type: string = '', filters: Object = {}) {
+    // If feed is requested but user is not authenticated, redirect to login
+    if (type === 'feed' && !this.isAuthenticated) {
+      this.router.navigateByUrl('/login');
+      return;
+    }
+
+    // Otherwise, set the list object
+    this.listConfig = {type: type, filters: filters};
+  }
+}
